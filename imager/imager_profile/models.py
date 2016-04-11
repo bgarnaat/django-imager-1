4974ad97d7e23346c822f6dc265a5c75ad8a67d3
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-11 15:05:58 -0700

    updated imager models

--- imager/imager_profile/models.py
+++ imager/imager_profile/models.py
@@ -1,11 +1,15 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
 
 
 PHOTOGRAPHY_TYPES = [
     ('portrait', 'Portrait'),
     ('landscape', 'Landscape'),
     ('sports', 'Sports'),
+    ('bw', 'Black and White'),
+    ('nature', 'Nature'),
 ]
 REGIONS = [
     ('pnw', 'Pacific Northwest'),
@@ -21,8 +25,22 @@ REGIONS = [
 ]
 
 
+@python_2_unicode_compatible
 class ImagerProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         related_name='profile'
+    )
+    camera_type = models.CharField(max_length=255)
+    type_of_photography = models.CharField(
+        max_length=128,
+        choices=PHOTOGRAPHY_TYPES,
+    )
+    friends = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='friend_of'
+    )
+    region = models.CharField(
+        max_length=3,
+        choices=REGIONS,
     )
