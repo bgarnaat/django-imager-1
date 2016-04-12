8822ef9c7d08ee494b5857dccab57a12cfb73b64
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-11 22:50:38 -0700

    added handlers and tests for user

--- imager/imager_profile/apps.py
+++ imager/imager_profile/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ImagerProfileConfig(AppConfig):
     name = 'imager_profile'
+
+    def ready(self):
+        """To run when app is ready."""
+        from imager_profile import handlers
--- imager/imager_profile/tests.py
+++ imager/imager_profile/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from imager_profile.models import ImagerProfile
+from django.contrib.auth.models import User
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Set up User Factory."""
+    class Meta:
+        model = User
+
+    username = 'Joe'
+
+
+class UserProfileTest(TestCase):
+    """Test user profiles."""
+
+    def setUp(self):
+        """Initialize a test user."""
+        self.user = UserFactory.create()
+        self.user.save()
+
+    def test_user_profile(self):
+        """Test if user has a profile."""
+        self.assertIsInstance(self.user.profile, ImagerProfile)
+    #
+    # def test_user_profile_active(self):
+    #     """Test user profile active."""
+    #     self.assertTrue(self.user.profile.is_active)
