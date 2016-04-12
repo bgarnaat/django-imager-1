95c9ec2d91be24aad8e4e71eee52639780581ffc
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-11 23:33:30 -0700

    trying to test for friends

--- imager/imager_profile/tests.py
+++ imager/imager_profile/tests.py
@@ -9,7 +9,8 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = 'Bob'
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
 
 
 class UserProfileTest(TestCase):
@@ -26,6 +27,21 @@ class UserProfileTest(TestCase):
     def test_user_profile_active(self):
         """Test user profile active."""
         self.assertTrue(self.user.profile.is_active)
+
+    def test_user_profile_camera(self):
+        """Test user profile camera field."""
+        self.user.camera_type = 'Holga'
+        self.assertEqual(self.user.camera_type, 'Holga')
+
+    def test_user_profile_region(self):
+        """Test user profile region."""
+        self.user.region = 'mtw'
+        self.assertEqual(self.user.region, 'mtw')
+
+    def test_user_profile_type_of_photography(self):
+        """Test user profile type of photography."""
+        self.user.type_of_photography = 'bw'
+        self.assertEqual(self.user.type_of_photography, 'bw')
 
     def test_user_delete(self):
         """Test for deleting users."""
