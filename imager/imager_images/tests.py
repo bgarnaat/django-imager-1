1b9235d374c134ee38f6910edbdc421a524a7f6d
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-12 00:09:42 -0700

    updated tests for photos and album

--- imager/imager_images/tests.py
+++ imager/imager_images/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.db.models import ImageField
+from django.db.models.fields.files import ImageFieldFile
+from imager_images.models import Photo, Album
+from imager_profile.models import ImagerProfile
+from imager_profile.tests import UserFactory
+import factory
 
-# Create your tests here.
+
+class PhotoFactory(factory.django.DjangoModelFactory):
+    """Set up Photo Factory."""
+    class Meta:
+        model = Photo
+    image = factory.django.ImageField(filename='/tmp/image01.jpg')
+    # date_created = '2016-04-11'
+
+
+class PhotoTest(TestCase):
+    """Test photo models."""
+
+    def setUp(self):
+        """Initialize a photo."""
+        self.user = UserFactory.create()
+        self.photo = PhotoFactory.create(
+            owner=self.user,
+        )
+
+    def test_photo_title(self):
+        """Test photo title field."""
+        self.photo.title = 'Outside'
+        self.assertEqual(self.photo.title, 'Outside')
