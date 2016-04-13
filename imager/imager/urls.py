9c5bed78c62b04b00375f0fd20a32afddbe672a7
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-13 15:36:21 -0700

    added registration/login.html form

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from .views import home_page
+# from .views import home_page
 from .views import ClassView
 from django.conf.urls.static import static
 from imager import settings
+# from django.contrib.auth.views import login
 
 # image_urls = []
 # profile_urls = []
@@ -28,7 +29,9 @@ from imager import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/(?P<id>[0-9]+)$', ClassView.as_view(), name='home_page'),
+    url(r'^$', ClassView.as_view(), name='home_page'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    # url(r'^registration/(?P<id>[0-9]+)$', ClassView.as_view(), name='home_page'),
     # url(r'^home/(?P<id>[0-9]+)$', home_page, name='home_page'),
     # url(r'^profile/', imager_profile.urls),
 ]
--- imager/imager/views.py
+++ imager/imager/views.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 # from django.template import loader
 from django.shortcuts import render
 from django.views.generic import TemplateView
+# from django.contrib.auth import login, logout
+from imager.settings import MEDIA_URL
+from imager_images.models import Photo
+# from django.contrib.auth.views import login
+import os
 
 
 def home_page(request, * args, **kwargs):
@@ -15,6 +20,11 @@ def home_page(request, * args, **kwargs):
 class ClassView(TemplateView):
     template_name = 'home.html'
 
-    def get_context_data(self, *args, **kwargs):
-        foo = 'garbanzo beans'
-        return {'foo': foo}
+    def get_context_data(self):
+        try:
+            img = Photo.objects.all().filter(
+                published='public').order_by('?')[0]
+            img_url = img.image.url
+        except IndexError:
+            img_url = os.path.join(MEDIA_URL, 'neil.jpg')
+        return {'img_url': img_url}
