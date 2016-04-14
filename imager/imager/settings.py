3be98e1dc761f9544630d9a31c767c7039fe9ebe
Ben Garnaat <garnaat.ben@gmail.com>
2016-04-14 15:32:44 -0700

    updated settings to use env_vars updated requirements.txt with additional requirements

--- imager/imager/settings.py
+++ imager/imager/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = 'imager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'imagerdb',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PW'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
