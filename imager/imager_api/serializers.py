03749a1401b936738c55c68eaccf6719823fad03
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-29 16:25:59 -0700

    tests for api views written

--- imager/imager_api/serializers.py
+++ imager/imager_api/serializers.py
@@ -5,12 +5,8 @@ from imager_images.models import Photo, Album
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        # fields = ('image', 'owner', 'title', 'description', 'date_uploaded',
-        #           'date_modified', 'date_published', 'published',)
 
 
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        # fields = ('image', 'owner', 'title', 'description', 'date_uploaded',
-        #           'date_modified', 'date_published', 'published',)
